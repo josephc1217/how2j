984422f45e07243ee33473a8e83be3303da4faa3
LAPTOP-J83EMR6A\chenq <qiaojiang.chen@juphoon.com.cn>
2021-04-16 17:03:30 +0800

    Add internal static class

--- src/character/AP.java
+++ src/character/AP.java
@@ -6,4 +6,8 @@ public interface AP {
     //reistMagic即使没有声明为public statis final, 但仍然模式是public static final.
     int resistMagic = 0;
     public void magicAttack();
+
+    default public void attack(){
+        System.out.println("ap attack default");
+    }
 }
