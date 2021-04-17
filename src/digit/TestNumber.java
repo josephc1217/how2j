a4c08e262aeccdf5ac4f7c8cef9fd609957c7927
josephc1217 <josephc1217@163.com>
2021-04-17 21:47:19 +0800

    格式化

--- src/digit/TestNumber.java
+++ src/digit/TestNumber.java
@@ -2,19 +2,14 @@ package digit;
 
 public class TestNumber {
     public static void main(String[] args){
+        int year = 2019;
 
-        String name = "盖伦";
-        int kill = 8;
-        String title = "超神";
+        System.out.printf("%d%n",year);
+        System.out.printf("%8d%n",year);
+        System.out.printf("%-8d%n",year);
+        System.out.printf("%08d%n",year);
+        System.out.printf("%,8d%n",year*1000);
+        System.out.printf("%.2f%n", Math.PI);
 
-        //直接使用+进行字符串连接，编码感觉
-        String sentence = name + " 在进行了连续 " + kill + " 次击杀后，获得了 " + title + " 的称号。";
-        System.out.println(sentence);
-
-        String sentenceFormat = "%s 在进行了连续 %d 次击杀后，获得了 %s 的称号。";
-        System.out.printf(sentenceFormat,name,kill,title);
-        System.out.format(sentenceFormat,name,kill,title);
-        System.out.printf("这是换行符%n");
-        System.out.printf("这是换行符%n");
     }
 }
