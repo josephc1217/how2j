ebddec1b45931b7dba50a907f1981732c66367b9
josephc1217 <josephc1217@163.com>
2021-04-17 20:02:40 +0800

    数组排序和比较

--- src/Test/HelloWorld.java
+++ src/Test/HelloWorld.java
@@ -5,24 +5,25 @@ import java.util.Collections;
 
 public class HelloWorld {
     public static void main(String[] args){
-        int[] a;
-        a = new int[5];
-        System.out.println("数组长度是 " + a.length);
+        int[] a = new int[]{18, 62, 68, 82, 65, 9 };
+        int[] c = new int[]{18, 62, 68, 82, 65, 8 };
+        int[] b = Arrays.copyOfRange(a,0,3);
 
-        for (int i=0; i<a.length; i++){
-            a[i] = (int)(Math.random()*100);
-            System.out.println(a[i]);
+        for(int i = 0; i<b.length; i++){
+            System.out.println(b[i]);
         }
 
-        System.out.println("本练习的目的是找出最小的一位");
-        int min = a[0];
-        for (int i = 0; i < a.length; i++){
-            if(a[i] < min){
-                min = a[i];
-            }
-        }
+        System.out.println("a数组排序之前：");
+        String content = Arrays.toString(a);
+        System.out.println(content);
 
-        System.out.println("最小值是：" + min);
+        System.out.println("a数组排序之后：");
+        Arrays.sort(a);
+        content = Arrays.toString(a);
+        System.out.println(content);
+
+        System.out.println("数字62出现的位置" + Arrays.binarySearch(a,62));
+        System.out.println("a数组和c输出是否相同？" + Arrays.equals(a,c));
 
     }
 }
