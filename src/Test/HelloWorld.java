9b0ef876bbd45a51fd7bdc4b131d9685b89e1829
josephc1217 <josephc1217@163.com>
2021-04-17 21:12:23 +0800

    字符串转换

--- src/Test/HelloWorld.java
+++ src/Test/HelloWorld.java
@@ -5,25 +5,8 @@ import java.util.Collections;
 
 public class HelloWorld {
     public static void main(String[] args){
-        int[] a = new int[]{18, 62, 68, 82, 65, 9 };
-        int[] c = new int[]{18, 62, 68, 82, 65, 8 };
-        int[] b = Arrays.copyOfRange(a,0,3);
-
-        for(int i = 0; i<b.length; i++){
-            System.out.println(b[i]);
-        }
-
-        System.out.println("a数组排序之前：");
-        String content = Arrays.toString(a);
-        System.out.println(content);
-
-        System.out.println("a数组排序之后：");
-        Arrays.sort(a);
-        content = Arrays.toString(a);
-        System.out.println(content);
-
-        System.out.println("数字62出现的位置" + Arrays.binarySearch(a,62));
-        System.out.println("a数组和c输出是否相同？" + Arrays.equals(a,c));
-
+        int[] a = new int[10];
+        Arrays.fill(a,5);
+        System.out.println(Arrays.toString(a));
     }
 }
--- src/digit/TestNumber.java
+++ src/digit/TestNumber.java
@@ -2,21 +2,22 @@ package digit;
 
 public class TestNumber {
     public static void main(String[] args){
+        int i = 5;
+        String str = String.valueOf(i);
 
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
-        System.out.println(Byte.MIN_VALUE);
-        System.out.println(Byte.MAX_VALUE);
+        Integer it = i;
+        String str2 = it.toString();
 
-        byte b1;
-        Byte b2 = 11;
-        Integer it = 12;
-        b1 = (byte)it.intValue();
-        b1 = b2.byteValue();
+        String str3 = "999";
+        int j = Integer.parseInt(str3);
+        System.out.println("j的值为： "+j);
 
-        short s1;
-        Short s2 = 123;
-        s1 = s2.shortValue();
+        float pai = (float)3.14;
+
+        System.out.println(Float.toString(pai));
+        String str4  = Float.toString(pai);
+        float f1 = Float.parseFloat(str4);
+
 
     }
 }
